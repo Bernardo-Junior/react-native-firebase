59827c58fb66005542697e9c18b8a600c294ed35
Bernardo <bernardo@iMac-Pro-de-Bernardo.local>
2021-08-08 21:38:50 -0400

    feat: adicionando comentários nos listeners

--- index.js
+++ index.js
@@ -8,14 +8,17 @@ import App from './src/app';
 import { name as appName } from './app.json';
 import messaging from '@react-native-firebase/messaging';
 
+// Esse é o listener que será ouvido quando o aplicativo estiver em segundo plano. (Background)
 messaging().onNotificationOpenedApp(async remoteMessage => {
   console.log(remoteMessage.data);
 });
 
+// Esse é o listener que será ouvido quando o aplicativo estiver fechado totalmente. (Quit)
 messaging().setBackgroundMessageHandler(async remoteMessage => {
   console.log(remoteMessage.data);
 })
 
+// Esse listener será ouvido caso o aplicativo seja aberto por outros meios.
 messaging()
   .getInitialNotification()
   .then(remoteMessage => {
@@ -25,12 +28,14 @@ messaging()
 
   });
 
+
+// Esse listener será ouvido caso a notificação chegue com o app aberto. (Foreground)
 messaging().onMessage(async remoteMessage => {
   console.log(remoteMessage.data);
 });
 
 
-function HeadlessCheck({ isHeadless }) {
+const HeadlessCheck =  ({ isHeadless }) => {
   if (isHeadless) {
     // App has been launched in the background by iOS, ignore
     return null;
